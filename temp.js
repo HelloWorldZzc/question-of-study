73b866dbec27285819ba7ff3b23e5dd68cd1720d
zzc <1393407976@qq.com>
2022-09-13 22:32:38 +0800

    22-9-13

--- temp.js
+++ temp.js
@@ -1,2 +1,11 @@
-console.log(4>=6&&"bbb")
-
+var str = 'acdaecad';
+var obj = {};
+for (var i = 0;i<str.length;i++){
+    if (obj[str.charAt(i)]){
+        obj[str.charAt(i)]++;
+    }
+    else {
+        obj[str.charAt(i)] = 1;
+    }
+}
+console.log(str.charAt(1))

b7a70af9a49cffd09cc4c9d75ab5570e2b11b843
zzc <1393407976@qq.com>
2022-09-20 22:50:17 +0800

    22-9-20

--- temp.js
+++ temp.js
@@ -1,3 +1,11 @@
-console.log(null>0)
-console.log(null==0)
-console.log(null<0)
+
+
+let number="-122323552"
+const reg=/(\d+)(\d{3})/
+while (reg.test(number)){
+    number=number.replace(reg,"$1,$2")
+}
+console.log(number)
+
+let arr=new Array(4).fill(1,1,2)
+console.log(arr)

b72b250f3d7dcfa3713dcdeb0b799339037587d0
zzc <1393407976@qq.com>
2022-09-22 23:01:59 +0800

    22-9-22

--- temp.js
+++ temp.js
@@ -1,12 +1,3 @@
-
-
-let number="-122323552"
-const reg=/(\d+)(\d{3})/
-while (reg.test(number)){
-    number=number.replace(reg,"$1,$2")
-}
-console.log(number)
-
-
-let arr=new Array(4).fill(1,1,2)
-console.log(arr)
+var a={}
+b=Object.prototype
+console.log([a.prototype===b,Object.getPrototypeOf(a)===b])
